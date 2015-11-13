57f5fa2af92831b7b9c897537dc16dde83792e16
Phil Manijak <github@exclsr.com>
2015-11-12 19:15:21 -0800

    Add basic sign-in page
    Add a sign-in page that accepts member
    credentials and then shows whether or
    not to sign-in request was a success.

---
+++ app/www/js/SignInCtrl.js
@@ -0,0 +1,46 @@
+angular.module('app.controllers', [])
+.controller('SignInCtrl', function ($scope, $http) {
+	$scope.member = {};
+	var server = "http://localhost:3000";
+
+	// TODO: Move to a library, share with client (website)
+	var signIn = function (email, password, callback) {
+
+        var xsrf = "";
+        xsrf += encodeURIComponent("email") + "=" + encodeURIComponent(email) + "&";
+		xsrf += encodeURIComponent("password") + "=" + encodeURIComponent(password);
+
+        var request = {
+            method: 'POST',
+            url: server + '/auth/signin',
+            data: xsrf,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        };      
+
+        $http(request)
+        .success(function (user, status) {
+            callback(null, user);
+        })
+        .error(function (data, status) {
+            var err = new Error(data)
+            err.status = status;
+            callback(err);
+        });
+    };
+
+	$scope.signIn = function (email, password) {
+		$scope.message = "";
+
+		signIn(email, password, function (err, member) {
+			if (err) {
+				switch (err.status) {
+					case 401:
+						$scope.message = "Sorry, please try something else.";
+				}
+				console.log(err);
+				return;
+			}
+			$scope.message = "Success!";
+		});
+	};
+});
