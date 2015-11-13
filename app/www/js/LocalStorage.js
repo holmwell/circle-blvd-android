9cecd0b370536f8bdca53edf32d95584ce4a6c32
Phil Manijak <github@exclsr.com>
2015-11-13 15:03:21 -0800

    Refactor: Put LocalStorage in own file

---
+++ app/www/js/LocalStorage.js
@@ -0,0 +1,20 @@
+angular.module('starter.services')
+
+// LocalStorage service suggestion from Ionic
+// http://learn.ionicframework.com/formulas/localstorage/
+.factory('LocalStorage', ['$window', function ($window) {
+  return {
+    set: function (key, value) {
+      $window.localStorage[key] = value;
+    },
+    get: function (key, defaultValue) {
+      return $window.localStorage[key] || defaultValue;
+    },
+    setObject: function (key, value) {
+      $window.localStorage[key] = JSON.stringify(value);
+    },
+    getObject: function (key) {
+      return JSON.parse($window.localStorage[key] || '{}');
+    }
+  }
+}]);
