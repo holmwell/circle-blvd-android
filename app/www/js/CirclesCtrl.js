e5b2a47df4287dba86f7a3e92b6c698b32212ad9
Phil Manijak <github@exclsr.com>
2015-11-13 12:00:35 -0800

    Show list of circles on Circles page

--- app/www/js/CirclesCtrl.js
+++ app/www/js/CirclesCtrl.js
@@ -1,7 +1,25 @@
 angular.module('starter.controllers')
 .controller('CirclesCtrl', function ($scope, $http, $state, Session) {
 	
-	var server = "http://localhost:3000";
-	console.log(Session.member);
+//	var server = "http://localhost:3000";
+	var member = Session.member;
+	if (!member) {
+		// TODO: Not signed in. Figure it out.
+		console.log("Member not set");
+		return;
+	}
 
+	var circles = {};
+
+	var membership;
+	var memberships = member.memberships;
+
+	member.memberships.forEach(function (membership) {
+		circles[membership.circleName] = {
+			circleName: membership.circleName,
+			circle: membership.circle
+		};
+	});
+
+	$scope.circleList = circles;
 });
